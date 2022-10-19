84949a6ed1d1270a00defce788b16999b2f0df0f
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-10-19 08:29:43 +0100

    refactor test

--- roman-numerals-kata-tests/RomanNumeralConverterShouldTests.cs
+++ roman-numerals-kata-tests/RomanNumeralConverterShouldTests.cs
@@ -3,53 +3,19 @@ using roman_numerals_kata;
 
 namespace roman_numerals_kata_tests {
     public class RomanNumeralConverterShouldTests {
-        [Test]
-        public void return_I_for_1()
-        {
-            var result = RomanNumeralConverter.ToRomanNumeral(1);
-            result.Should().Be("I");
-        }
 
-        [Test]
-        public void return_II_for_2()
-        {
-            var result = RomanNumeralConverter.ToRomanNumeral(2);
-            result.Should().Be("II");
-        }
 
-        [Test]
-        public void return_III_for_3()
+        [TestCase(1,"I")]
+        [TestCase(2,"II")]
+        [TestCase(3,"III")]
+        [TestCase(4,"IV")]
+        [TestCase(5,"V")]
+        [TestCase(6,"VI")]
+        [TestCase(7,"VII")]
+        public void return_roman_numeral_for(int number, string expected)
         {
-            var result = RomanNumeralConverter.ToRomanNumeral(3);
-            result.Should().Be("III");
-        }
-
-        [Test]
-        public void return_IV_for_4()
-        {
-            var result = RomanNumeralConverter.ToRomanNumeral(4);
-            result.Should().Be("IV");
-        }
-
-        [Test]
-        public void return_V_for_5()
-        {
-            var result = RomanNumeralConverter.ToRomanNumeral(5);
-            result.Should().Be("V");
-        }
-
-        [Test]
-        public void return_VI_for_6()
-        {
-            var result = RomanNumeralConverter.ToRomanNumeral(6);
-            result.Should().Be("VI");
-        }
-
-        [Test]
-        public void return_VII_for_7()
-        {
-            var result = RomanNumeralConverter.ToRomanNumeral(7);
-            result.Should().Be("VII");
+            var result = RomanNumeralConverter.ToRomanNumeral(number);
+            result.Should().Be(expected);
         }
     }
 }
