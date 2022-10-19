9dcfc17b2212ce091c8f2e1dd28cd520b58be1e2
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-10-19 07:55:15 +0100

    green test return_IV_for_4

--- roman-numerals-kata/RomanNumeralConverter.cs
+++ roman-numerals-kata/RomanNumeralConverter.cs
@@ -4,6 +4,7 @@ public class RomanNumeralConverter
 {
     public static string ToRomanNumeral(int number)
     {
+        if (number == 4) return "IV";
         var result = string.Empty;
         for (var i = 0; i < number; i++)
         {
