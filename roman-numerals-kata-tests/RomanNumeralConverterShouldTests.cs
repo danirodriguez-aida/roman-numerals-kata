dd64baf24d1a0480962ac0c4b7a05497806933d0
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-10-19 08:32:55 +0100

    red test return_roman_numeral_for 9

--- roman-numerals-kata-tests/RomanNumeralConverterShouldTests.cs
+++ roman-numerals-kata-tests/RomanNumeralConverterShouldTests.cs
@@ -13,6 +13,7 @@ namespace roman_numerals_kata_tests {
         [TestCase(6,"VI")]
         [TestCase(7,"VII")]
         [TestCase(8,"VIII")]
+        [TestCase(9,"IX")]
         public void return_roman_numeral_for(int number, string expected)
         {
             var result = RomanNumeralConverter.ToRomanNumeral(number);
