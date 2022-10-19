8d2600182b6582e5fd26982252b5aa5191c1e16b
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-10-19 07:44:49 +0100

    red test return_I_for_1

--- roman-numerals-kata-tests/RomanNumeralConverterShouldTests.cs
+++ roman-numerals-kata-tests/RomanNumeralConverterShouldTests.cs
@@ -1,9 +1,20 @@
+using FluentAssertions;
+
 namespace roman_numerals_kata_tests {
     public class RomanNumeralConverterShouldTests {
         [Test]
-        public void Test1() {
-            
+        public void return_I_for_1()
+        {
+            var result = RomanNumeralConverter.ToRomanNumeral(1);
+            result.Should().Be("I");
+        }
+    }
 
+    public class RomanNumeralConverter
+    {
+        public static string ToRomanNumeral(int number)
+        {
+            throw new NotImplementedException();
         }
     }
 }
