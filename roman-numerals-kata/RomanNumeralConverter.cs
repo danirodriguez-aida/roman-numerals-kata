cd037f0939402ecadcbee4c7cde5f1e0a746477d
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-10-19 07:49:56 +0100

    green test return_III_for_3

--- roman-numerals-kata/RomanNumeralConverter.cs
+++ roman-numerals-kata/RomanNumeralConverter.cs
@@ -5,6 +5,7 @@ public class RomanNumeralConverter
     public static string ToRomanNumeral(int number)
     {
         if (number == 2) return "II";
+        if (number == 3) return "III";
         return "I";
     }
 }
