2b6cc68a1a5e66b9e03035a34689542fefe7bbe4
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-10-19 08:00:18 +0100

    green test return_VI_for_6

--- roman-numerals-kata/RomanNumeralConverter.cs
+++ roman-numerals-kata/RomanNumeralConverter.cs
@@ -6,6 +6,7 @@ public class RomanNumeralConverter
     {
         if (number == 4) return "IV";
         if (number == 5) return "V";
+        if (number == 6) return "VI";
         var result = string.Empty;
         for (var i = 0; i < number; i++)
         {
