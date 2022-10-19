a21e4d0bcc868f72c256608486831cfe93c67a05
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-10-19 08:19:55 +0100

    refactor

--- roman-numerals-kata/RomanNumeralConverter.cs
+++ roman-numerals-kata/RomanNumeralConverter.cs
@@ -4,11 +4,13 @@ public class RomanNumeralConverter
 {
     public static string ToRomanNumeral(int number)
     {
-        if (number == 4) return "IV";
-        if (number == 5) return "V";
-        if (number == 6) return "VI";
-        if (number == 7) return "VII";
         var result = string.Empty;
+        if (number == 4) return "IV";
+        if (number >= 5)
+        {
+            result = "V";
+            number -= 5;
+        }
         for (var i = 0; i < number; i++)
         {
             result += "I";
