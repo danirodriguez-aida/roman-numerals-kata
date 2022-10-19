cd1a6fc463a622ea4e47fa7f5f046c0e61a5279b
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-10-19 08:44:02 +0100

    green test return_roman_numeral_for 11

--- roman-numerals-kata/RomanNumeralConverter.cs
+++ roman-numerals-kata/RomanNumeralConverter.cs
@@ -7,6 +7,7 @@ public class RomanNumeralConverter
         var result = string.Empty;
         if (number == 9) return "IX";
         if (number == 10) return "X";
+        if (number == 11) return "XI";
         if (number == 4) return "IV";
         if (number >= 5)
         {
