d6c7325014c1f5014e11cf4a84d0b7be2c95c2ff
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-10-21 11:49:37 +0100

    refactor

--- roman-numerals-kata/RomanNumeralConverter.cs
+++ roman-numerals-kata/RomanNumeralConverter.cs
@@ -3,7 +3,6 @@ namespace roman_numerals_kata;
 public class RomanNumeralConverter
 {
     private int _number;
-
     private static readonly Dictionary<int, string> RomanNumerals = new()
     {
         { 1, "I" },
@@ -14,6 +13,19 @@ public class RomanNumeralConverter
         { 500, "D" },
         { 1000, "M" },
     };
+   
+    private class Multipliers
+    {
+        public static int Ten => 1;
+        public static int OneHundred => 10;
+        public static int OneThousand => 100;
+        //public static IList<int> GetMultipliers() => new List<int>()
+        //{
+        //    OneThousand,
+        //    OneHundred,
+        //    Ten
+        //};
+    }
 
     public RomanNumeralConverter(int number)
     {
@@ -23,36 +35,44 @@ public class RomanNumeralConverter
     {
         var result = string.Empty;
         var romanNumeralConverter = new RomanNumeralConverter(number);
-        result += romanNumeralConverter.ProcessNumberLess(1000);
-        result += romanNumeralConverter.ProcessNumberLess(100);
-        result += romanNumeralConverter.ProcessNumberLess(10);
+        result += romanNumeralConverter.ProcessNumberLess(Multipliers.OneThousand);
+        result += romanNumeralConverter.ProcessNumberLess(Multipliers.OneHundred);
+        result += romanNumeralConverter.ProcessNumberLess(Multipliers.Ten);
         return result;
     }
     private string ProcessNumberLess(int multiplier)
     {
         var result = string.Empty;
-        multiplier = (multiplier / 10);
-        if (_number >= (9 * multiplier))
+        var multiplierOf1 = 1 * multiplier;
+        var multiplierOf4 = 4 * multiplier;
+        var multiplierOf5 = 5 * multiplier;
+        var multiplierOf9 = 9 * multiplier;
+        var multiplierOf10 = 10 * multiplier;
+        if (_number >= multiplierOf9)
         {
-            _number -= (9 * multiplier);
-            return RomanNumerals[1 * multiplier] + RomanNumerals[10 * multiplier];
+            _number -= multiplierOf9;
+            result += RomanNumerals[multiplierOf1] + RomanNumerals[multiplierOf10];
         }
-        if (_number >= (5* multiplier))
+        if (_number >= multiplierOf5)
         {
-            _number -= (5* multiplier);
-            result += RomanNumerals[5* multiplier];
+            result +=  ConvertToNumeral(multiplierOf5);
+        }
+        if (_number >= multiplierOf4)
+        {
+            _number -= multiplierOf4;
+            result += RomanNumerals[multiplierOf1] + RomanNumerals[multiplierOf5];
         }
 
-        if (_number >= (4* multiplier))
+        while (_number >= multiplierOf1)
         {
-            _number -= (4* multiplier);
-            return RomanNumerals[1* multiplier] + RomanNumerals[5* multiplier];
-        }
-        while (_number >= (1* multiplier))
-        {
-            _number -= (1* multiplier);
-            result += RomanNumerals[1* multiplier];
+            result += ConvertToNumeral(multiplierOf1);
         }
         return result;
+    }
+
+    private string ConvertToNumeral(int number)
+    {
+        _number -= number;
+        return RomanNumerals[number];
     }
 }
